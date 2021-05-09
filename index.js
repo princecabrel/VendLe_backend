f96a3e58b8bde2c501749bd2156846487866f282
Gaetan-M <mopidaryl@gmail.com>
2021-05-09 23:18:47 +0100

    endpoints of Login and Register

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express =require('express');
 const app=express();
 const bodyParser=require('body-parser');
 const cors=require('cors');
-
+const dbConnect = require('./db.connect.js');
+const User=require('./routes/User.route.js');
 /*****cors error protection and data parsing*****/
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,8 +15,8 @@ app.use(cors());
 app.use(bodyParser.json({limit:"50mb",extended: true, parameterLimit:50000}));
 
 /*******endpoints******/
-app.use('/',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
 	res.json('Hello world !')
 })
-
+app.use('/',User);
 module.exports=app
